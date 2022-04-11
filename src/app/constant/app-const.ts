5835ceaf28966cf4f3187ac5ea419ee2cf30d0a4
shapartha <chat.connect.me@gmail.com>
2022-04-12 01:31:48 +0530

    Issue Fix with Google Account lost permission

--- src/app/constant/app-const.ts
+++ src/app/constant/app-const.ts
@@ -1,6 +1,6 @@
 export class AppConstant {
     static RUPEE_SYMBOL: string = "₹";
-    static APP_VERSION: string = "5.72";
+    static APP_VERSION: string = "5.73";
     static MONTH: any = {
         1: "Jan",
         2: "Feb",
